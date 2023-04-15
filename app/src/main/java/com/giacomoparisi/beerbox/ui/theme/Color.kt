399dbfd686f383f22e0565f10292bdee05a92f8a
Giacomo Parisi <giacomoparisi95@gmail.com>
2023-04-15 13:59:22 +0200

    Updated app theme colors

--- app/src/main/java/com/giacomoparisi/beerbox/ui/theme/Color.kt
+++ app/src/main/java/com/giacomoparisi/beerbox/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.giacomoparisi.beerbox.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val WhiteFFFFFF = Color(color = 0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val YellowF2B14E = Color(color = 0xFFF2B14E)
+
+val Black0E181C = Color(color = 0xFF0E181C)
+val Black272524  = Color(color = 0xFF272524)
+val Black1D262B  = Color(color = 0xFF1D262B)
+
+val Gray8C9194 = Color(color = 0xFF8C9194)
--- app/src/main/java/com/giacomoparisi/beerbox/ui/theme/Theme.kt
+++ app/src/main/java/com/giacomoparisi/beerbox/ui/theme/Theme.kt
@@ -1,69 +1,26 @@
 package com.giacomoparisi.beerbox.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+private val DarkColorScheme =
+    darkColorScheme(
+        primary = YellowF2B14E,
+        onPrimary = Black272524,
+        secondary = Black1D262B,
+        onSecondary = Gray8C9194,
+        background = Black0E181C,
+        onBackground = WhiteFFFFFF,
+    )
 
 @Composable
 fun BeerBoxTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
+    // Use Only DarkColorScheme for this Example
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = DarkColorScheme,
         typography = Typography,
         content = content
     )
