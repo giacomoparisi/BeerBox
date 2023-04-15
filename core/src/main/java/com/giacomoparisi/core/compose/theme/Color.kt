c2d813815808ef08a74a70bb6ae45a1cfb24ad03
Giacomo Parisi <giacomoparisi95@gmail.com>
2023-04-15 16:18:01 +0200

    Moved theme class to core module

--- core/src/main/java/com/giacomoparisi/core/compose/theme/Color.kt
+++ core/src/main/java/com/giacomoparisi/core/compose/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.giacomoparisi.beerbox.ui.theme
+package com.giacomoparisi.core.compose.theme
 
 import androidx.compose.ui.graphics.Color
 
--- core/src/main/java/com/giacomoparisi/core/compose/theme/Theme.kt
+++ core/src/main/java/com/giacomoparisi/core/compose/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.giacomoparisi.beerbox.ui.theme
+package com.giacomoparisi.core.compose.theme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
